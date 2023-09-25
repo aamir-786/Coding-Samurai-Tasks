624d635b4fc6647fdacb14669f0fc9c2f6d72e4f
Aamir Hussain <126471929+aamir-786@users.noreply.github.com>
2023-09-26 00:06:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-let inputs = document.getElementById("inp");
-let text = document.querySelector(".text");
 
-function Add(){
-    if(inputs.value == ""){
-        alert("Please Enter Task")
-    }else{
-        let newEle = document.createElement("ul");
-        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
-        text.appendChild(newEle);
-        inputs.value="";
-        newEle.querySelector("i").addEventListener("click" , remove);
-        function remove(){
-            newEle.remove()
-        }
+        let inputs = document.getElementById("inp");
+        let text = document.querySelector(".text");
+        let tasks = [];
+
+        // Load tasks from local storage when the page loads
+        window.onload = function () {
+        if (localStorage.getItem('tasks')) {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        renderTasks();
     }
-}
+    };
+
+        function renderTasks() {
+        text.innerHTML = '';
+        tasks.forEach(function (task, index) {
+        let newEle = document.createElement("ul");
+        newEle.innerHTML = `${task} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+
+        newEle.querySelector("i").addEventListener("click", function () {
+        removeTask(index);
+    });
+    });
+    }
+
+        function Add() {
+        if (inputs.value === "") {
+        alert("Please Enter Task");
+    } else {
+        tasks.push(inputs.value);
+        saveTasksToLocalStorage();
+        renderTasks();
+        inputs.value = "";
+    }
+    }
+
+        function removeTask(index) {
+        tasks.splice(index, 1);
+        saveTasksToLocalStorage();
+        renderTasks();
+    }
+
+        function saveTasksToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
